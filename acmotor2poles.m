25c525bd38ad76bf902ee25c52beaf2ecee2b6f7
Auralius Manurung <auralius@users.noreply.github.com>
2019-09-17 17:29:43 +0700

    First commit

---
+++ acmotor2poles.m
@@ -0,0 +1,113 @@
+% Auralius Manurung
+% manurunga@yandex.com
+
+clear all;
+close all;
+clc;
+
+filename = '2poles.gif';
+
+Im = 1; % The current, in Amps
+R = 0.5; % in m , this is the outer radius radius of the stator
+
+fig = figure;
+hold on;
+xlim([-R-R/10 R+R/10]);
+ylim([-R-R/10 R+R/10]);
+axis equal;
+
+step = 0.05;
+mu_0 = 4*pi*10^-7;
+
+% 2-pole ac motor
+% 2-pole motor has 6 wire position, one wire position to antoher makes a
+% 60 degress angle
+
+for wt = 0:360
+    Ia = Im*sind(wt-0);
+    Ia_ = -Im*sind(wt-0);
+    Ib = Im*sind(wt-120);
+    Ib_ = -Im*sind(wt-120);
+    Ic = Im*sind(wt-240);
+    Ic_ = -Im*sind(wt-240);
+    
+    xa = [0 -R 0]';
+    xa_= [0 R 0]';
+    xb = [R*cosd(30) R*sind(30) 0]';
+    xb_= [-R*cosd(30) -R*sind(30) 0]';
+    xc = [-R*cosd(30) R*sind(30) 0]';
+    xc_= [R*cosd(30) -R*sind(30) 0]';
+    
+    P = [xa xa_ xb xb_ xc xc_];
+    I = [Ia Ia_ Ib Ib_ Ic Ic_];
+    
+    index = 1;
+    
+    for x = -R:step:R
+        for y = -R:step:R
+            
+            B = [0;0;0];
+            
+            for k = 1 : length(P)
+                if I(k) ~= 0
+                    dL = [0;0;I(k)/abs(I(k))];
+                else
+                    dL = [0;0;0];
+                end
+                
+                r = [x;y;0] - P(:,k);
+                r_norm = norm(r);
+                
+                if r_norm == 0
+                    break;
+                end
+                
+                r_hat = r / r_norm;
+                
+                dB = abs(I(k)) * mu_0 / (4 * pi * r_norm^2) * cross(dL, r_hat);
+                B = B + dB;
+                
+            end
+            
+            if norm(B) > 1e-10
+                B_hat = B/norm(B);
+            else
+                B_hat=[0;0;0];
+            end
+            
+            X(index) = x;
+            Y(index) = y;
+            Z(index) = 0;
+            U(index) = B_hat(1);
+            V(index) = B_hat(2);
+            W(index) = B_hat(3);
+            
+            index = index + 1;
+        end
+    end
+    
+    frame = getframe(fig);
+    im = frame2im(frame);
+    [imind,cm] = rgb2ind(im,256);
+    
+    if wt == 0
+        h = quiver3(X,Y,Z, U, V, W);
+        plot(xa(1), xa(2), 'ro');
+        plot(xa_(1), xa_(2), 'ro');
+        plot(xb(1), xb(2), 'ro');
+        plot(xb_(1), xb_(2), 'ro');
+        plot(xc(1), xc(2), 'ro');
+        plot(xc_(1), xc_(2), 'ro');
+        
+        imwrite(imind,cm,filename,'gif', 'Loopcount',inf);
+    else
+        set(h,'xdata',X,'ydata',Y,'zdata',Z,'udata',U, 'vdata',V,'wdata',W)
+        drawnow
+        %pause(0.1);
+        
+        imwrite(imind,cm,filename,'gif','WriteMode','append');
+    end
+end
+
+
+
---
+++ acmotor4poles.m
@@ -0,0 +1,133 @@
+% Auralius Manurung
+% manurunga@yandex.com
+
+clear all;
+close all;
+clc;
+
+filename = '4poles.gif';
+
+Im = 1; % The current, in Amps
+R = 0.5; % in m , this is the outer radius radius of the stator
+
+fig = figure;
+hold on;
+xlim([-R-R/10 R+R/10]);
+ylim([-R-R/10 R+R/10]);
+axis equal;
+
+step = 0.05;
+mu_0 = 4*pi*10^-7;
+
+% 4-pole ac motor
+% 4-pole motor has 12 wire position, one wire position to antoher makes a
+% 30 degress angle
+
+for wt = 0:360
+    Ia1 = Im*sind(wt-0);
+    Ia1_ = -Im*sind(wt-0);
+    Ib1 = Im*sind(wt-120);
+    Ib1_ = -Im*sind(wt-120);
+    Ic1 = Im*sind(wt-240);
+    Ic1_ = -Im*sind(wt-240);
+    
+    Ia2 = Im*sind(wt-0);
+    Ia2_ = -Im*sind(wt-0);
+    Ib2 = Im*sind(wt-120);
+    Ib2_ = -Im*sind(wt-120);
+    Ic2 = Im*sind(wt-240);
+    Ic2_ = -Im*sind(wt-240);
+    
+    xa1 = [0 -R 0]';
+    xa1_= [R 0 0]';
+    xb1 = [R*cosd(30) -R*sind(30) 0]';
+    xb1_= [R*cosd(60) R*sind(60) 0]';
+    xc1 = [R*cosd(30) R*sind(30) 0]';
+    xc1_= [-R*cosd(60) R*sind(60) 0]';
+    
+    
+    xa2 = [0 R 0]';
+    xa2_= [-R 0 0]';
+    xb2 = [-R*cosd(30) R*sind(30) 0]';
+    xb2_= [-R*cosd(60) -R*sind(60) 0]';
+    xc2 = [-R*cosd(30) -R*sind(30) 0]';
+    xc2_= [R*cosd(60) -R*sind(60) 0]';
+    
+    P = [xa1 xa1_ xb1 xb1_ xc1 xc1_ xa2 xa2_ xb2 xb2_ xc2 xc2_];
+    I = [Ia1 Ia1_ Ib1 Ib1_ Ic1 Ic1_ Ia2 Ia2_ Ib2 Ib2_ Ic2 Ic2_];
+    
+    index = 1;
+    
+    for x = -R:step:R
+        for y = -R:step:R
+            
+            B = [0;0;0];
+            
+            for k = 1 : length(P)
+                if I(k) ~= 0
+                    dL = [0;0;I(k)/abs(I(k))];
+                else
+                    dL = [0;0;0];
+                end
+                
+                r = [x;y;0] - P(:,k);
+                r_norm = norm(r);
+                
+                if r_norm == 0
+                    break;
+                end
+                
+                r_hat = r / r_norm;
+                
+                dB = abs(I(k)) * mu_0 / (4 * pi * r_norm^2) * cross(dL, r_hat);
+                B = B + dB;
+                
+            end
+            
+            if norm(B) > 1e-10
+                B_hat = B/norm(B);
+            else
+                B_hat=[0;0;0];
+            end
+            
+            X(index) = x;
+            Y(index) = y;
+            Z(index) = 0;
+            U(index) = B_hat(1);
+            V(index) = B_hat(2);
+            W(index) = B_hat(3);
+            
+            index = index + 1;
+        end
+    end
+    
+    frame = getframe(fig);
+    im = frame2im(frame);
+    [imind,cm] = rgb2ind(im,256);
+    
+    if wt == 0
+        h = quiver3(X,Y,Z, U, V, W);
+        plot(xa1(1), xa1(2), 'ro');
+        plot(xa1_(1), xa1_(2), 'ro');
+        plot(xb1(1), xb1(2), 'ro');
+        plot(xb1_(1), xb1_(2), 'ro');
+        plot(xc1(1), xc1(2), 'ro');
+        plot(xc1_(1), xc1_(2), 'ro');
+        
+        plot(xa2(1), xa2(2), 'ro');
+        plot(xa2_(1), xa2_(2), 'ro');
+        plot(xb2(1), xb2(2), 'ro');
+        plot(xb2_(1), xb2_(2), 'ro');
+        plot(xc2(1), xc2(2), 'ro');
+        plot(xc2_(1), xc2_(2), 'ro')
+        
+        imwrite(imind,cm,filename,'gif', 'Loopcount',inf);
+    else
+        set(h,'xdata',X,'ydata',Y,'zdata',Z,'udata',U, 'vdata',V,'wdata',W)
+        drawnow
+        %pause(0.1);
+        
+        imwrite(imind,cm,filename,'gif','WriteMode','append');
+    end
+end
+
